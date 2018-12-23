e864ea233fdfe1beadcf91f8a0319c98bec6b43e
Jerry Lee <lzjjerrywoodylee@gmail.com>
2018-12-24 05:49:36 +0900

    delete test DB after test

--- tests/setup.js
+++ tests/setup.js
@@ -3,29 +3,11 @@ const ClipyMate = require('../index');
 const schema = require('../lib/schema');
 
 module.exports = async function (params) {
-  console.log('setup.js');
+  console.log('Run setup.js');
   // console.log(params);
 
   const clipy = new ClipyMate();
   clipy.realm = await Realm.open({ schema: Object.values(schema), schemaVersion: 7 });
   console.log(clipy.realm.schemaVersion);
   clipy.disconnect();
-
 };
-/*
-const sampleSnippet = {
-  title: 'sampleSnippet',
-  content: 'Sample Snippet',
-  index: 0,
-  identifier: uuid().toUpperCase(),
-  enable: true,
-}
-
-const sampleFolder = {
-  title: "sampleFolder",
-  snippets: [sampleSnippet],
-  index: 0,
-  identifier: uuid().toUpperCase(),
-  enable: true,
-}
-*/
