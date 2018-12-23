7c1657cd94a9c89071cc297cf40a66e9e9c0f6a7
Jerry Lee <lzjjerrywoodylee@gmail.com>
2018-12-24 04:51:37 +0900

    create dummy realm db

--- tests/setup.js
+++ tests/setup.js
@@ -1,4 +1,31 @@
+const Realm = require('realm');
+const ClipyMate = require('../index');
+const schema = require('../lib/schema');
+
 module.exports = async function (params) {
-  console.log('setup.js')
-  // console.log(params)
+  console.log('setup.js');
+  // console.log(params);
+
+  const clipy = new ClipyMate();
+  clipy.realm = await Realm.open({ schema: Object.values(schema), schemaVersion: 7 });
+  console.log(clipy.realm.schemaVersion);
+  clipy.disconnect();
+
+};
+/*
+const sampleSnippet = {
+  title: 'sampleSnippet',
+  content: 'Sample Snippet',
+  index: 0,
+  identifier: uuid().toUpperCase(),
+  enable: true,
 }
+
+const sampleFolder = {
+  title: "sampleFolder",
+  snippets: [sampleSnippet],
+  index: 0,
+  identifier: uuid().toUpperCase(),
+  enable: true,
+}
+*/
